e285d3b8fc1fe4f1107f6dd0074dc07fed403cba
Pranava Balugari <stalin.pranava@gmail.com>
2020-06-13 17:38:49 +1000

    Add ListProducts handler - json return type to the API endpoint

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
@@ -28,7 +28,7 @@ func main() {
 	*/
 	api := http.Server{
 		Addr:              "localhost:3000",
-		Handler:           http.HandlerFunc(Echo),
+		Handler:           http.HandlerFunc(ListProducts),
 		ReadTimeout:       5 * time.Second,
 		WriteTimeout:      5 * time.Second,
 	}
@@ -98,19 +98,37 @@ func main() {
 	}
 }
 
-func Echo(w http.ResponseWriter, r *http.Request) {
-	/*
-		--------------------------------------------
-		Generate a random number.
-		--------------------------------------------
-	*/
-	n := rand.Intn(2000)
-	log.Println("Staring number : ", n)
-	defer log.Println("Ending number : ", n)
+type Product struct {
+	Name	string `json:"name"`
+	Cost 	float64 `json:"cost"`
+	Quantity int64 `json:"quantity"`
+}
 
-	// Simulate a long-running request.
-	time.Sleep(1 * time.Second)
+func ListProducts(w http.ResponseWriter, r *http.Request) {
+	// This is not the real world scenario where we hardcode values
+	// products := []Product{}
+	// enchoding json will treat that as empty array rather than nil
+	// This helps FE to handle bugs efficiently.
+	products := []Product{
+		{ Name: "Geography books", Cost: 200.25, Quantity: 10},
+		{ Name: "History books", Cost: 300.25, Quantity: 15},
+		{ Name: "Civics books", Cost: 289.25, Quantity: 20},
+		{ Name: "Economics books", Cost: 100.25, Quantity: 25},
+		{ Name: "Physics books", Cost: 220.25, Quantity: 30},
+	}
 
-	fmt.Fprintf(w, "You asked to %s %s", r.Method, r.URL.Path)
+	data, err := json.Marshal(products)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	// We can explicitly set the header status Ok but it should always be set
+	// After the Header().set statements or else WriteHeader will override the values
+	// w.WriteHeader(http.StausOK)
+	if _, err := w.Write(data); err != nil {
+		log.Println("Error in writing the data ", err)
+	}
 }
 
